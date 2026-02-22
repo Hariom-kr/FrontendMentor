0f9cfc5d76f8d92ae2c152aad3d588340dc7612d
Hariom <hariomkr9128@gmail.com>
2026-02-23 01:40:35 +0530

    Fixed folder indexing issue

---
+++ Qrcard/src/App.jsx
@@ -0,0 +1,11 @@
+import QrCard from "./QrCard"
+
+const App = () => {
+  return (
+    <div className='bg-slate-300 h-screen flex justify-center  items-center'>
+     <QrCard/>
+    </div>
+  )
+}
+
+export default App
---
+++ Qrcard/src/QrCard.jsx
@@ -0,0 +1,20 @@
+import qrImage from "./assets/image-qr-code.png";
+
+const QrCard = () => {
+  return (
+    <div className='bg-white  rounded-2xl max-w-sm p-4
+    '>
+        <img src={qrImage} alt="QR" className="rounded-lg"/>
+        <div className="space-y-4">
+        <h2 className="font-bold text-center text-xl mt-2">
+         Improve your front-end <br/> 
+         Skills by building projects
+         </h2>
+         <p className=" text-sm text-slate-500  text-center mb-3">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default QrCard
